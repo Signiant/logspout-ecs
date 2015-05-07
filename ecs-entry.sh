07f08b1788b852ed87a5ce236aa10d1d99c24b23
Signiant Devops <devops@signiant.com>
2015-05-07 09:34:48 -0400

    Update ecs-entry.sh

--- ecs-entry.sh
+++ ecs-entry.sh
@@ -3,6 +3,7 @@
 # get the AWS instance ID
 INST=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 
+# append the instance ID to an already set syslog hostname
 if [ ! -z "$INST" ]; then
         if [ ! -z "$SYSLOG_HOSTNAME" ]; then
                 SYSLOG_HOSTNAME=${SYSLOG_HOSTNAME}_$INST
