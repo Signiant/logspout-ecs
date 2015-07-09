8938b971b58b1d96474e0ae169a67a3ee0ce1c06
Dave North <dnorth98@gmail.com>
2015-07-09 13:42:17 -0400

    Set a variable for allowing off AWS running

--- ecs-entry.sh
+++ ecs-entry.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # get the AWS instance ID
-INST=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+if [ ! -z "$OFF_AWS" ]; then
+        echo "Not running on AWS - will not get the instance ID"
+else
+        INST=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+fi
 
 # append the instance ID to an already set syslog hostname
 if [ ! -z "$INST" ]; then
